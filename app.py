3cc37216d5d2736e4be0903ea5442f24147fd978
safwanvk <safwanvalakundil@gmail.com>
2020-09-03 15:34:49 +0530

    :zap: Set Create todo

--- app.py
+++ app.py
@@ -1,11 +1,29 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+from flask_sqlalchemy import SQLAlchemy
+from werkzeug.utils import redirect
+
 
 app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///todo.sqlite"
+db = SQLAlchemy(app)
 
 
-@app.route('/')
+class Todo(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String, nullable=False)
+
+
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    return render_template('index.html')
+    if request.method == 'POST':
+        title = request.form['title']
+        new = Todo(title=title)
+        db.session.add(new)
+        db.session.commit()
+        return redirect('/')
+    else:
+        todo = Todo.query.all()
+        return render_template('index.html', todos=todo)
 
 
 if __name__ == "__main__":
